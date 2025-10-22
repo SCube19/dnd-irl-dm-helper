51a49a52ef17a0822a071967dfd1ece59213c6a3
SCube19 <SCube19>
2025-10-23 00:53:18 +0200

    MapScreen: Make a shader fog with skia
    Ticket: #1
    Ticket: #2
    Signed-off-by: SCube19 <SCube19>

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-import { registerRootComponent } from 'expo';
-
-import App from './App';
+import { registerRootComponent } from "expo";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
+
+//Change later to deferred load component
+LoadSkiaWeb().then(async () => {
+  const App = (await require("./App.tsx")).default;
+  registerRootComponent(App);
+});
